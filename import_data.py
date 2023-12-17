b145d9616bceeb8e208bedcc554c0309695af521
aokolowicz <adrian.okolowicz@gmail.com>
2023-12-17 21:16:36 +0100

    Added specific data import of total refresh

--- import_data.py
+++ import_data.py
@@ -3,27 +3,38 @@ import pathlib
 
 from aocd import get_data
 
-# Set up the argument parser
+# Set up argument parser
 parser = argparse.ArgumentParser()
 
 # Define optional command-line arguments
 parser.add_argument('-d', '--day', type=int, choices=range(1, 26))
 parser.add_argument('-y', '--year', type=int, choices=range(2015, 2050))
 
-# Parse the command-line arguments
+# Parse command-line arguments
 args = parser.parse_args()
 
-print(args.day, args.year)
-
+# If user provided command-line arguments
 if args.day and args.year:
-    day = f'{args.day}'
-    if args.day < 10:
-        day = f'0{day}'
+    # List all `input.txt` files for year and day
+    path = [
+        p
+        for p in pathlib.Path.cwd().glob(
+            f'**/{args.year}/{args.day:02d}*/input.txt'
+        )
+    ]
 
-    pathlib.Path.cwd().glob(f'**/{args.year}/{day}*/input.txt')
+    # There should be only one file for specific year and day
+    # Write text data to this file
+    path[0].write_text(get_data(day=args.day, year=args.year))
 
+# If no command-line arguments
 else:
-    # TODO: when no CL arguments - refresh data in all files
+    # Refresh data in all files
     for path in pathlib.Path.cwd().glob(f'**/input.txt'):
-        print(path)
-
+        day = (
+            path.parent.name[1:2]
+            if path.parent.name[0] == '0'
+            else path.parent.name[:2]
+        )
+        year = path.parent.parent.name
+        path.write_text(get_data(day=int(day), year=int(year)))
