b066b73e7a1824d21a48a55e0840dcb3101511c0
Adrian <130671043+aokolowicz@users.noreply.github.com>
2023-12-18 15:19:19 +0100

    Added automated submission

--- 2023/aoc202300_template.py
+++ 2023/aoc202300_template.py
@@ -1,14 +1,20 @@
 import pathlib
 import sys
 
+from aocd import submit
+
+
 def parse(puzzle_input):
     """Parse input."""
+
 
 def part1(data):
     """Solve part 1."""
 
+
 def part2(data):
     """Solve part 2."""
+
 
 def solve(puzzle_input):
     """Solve the puzzle for the given input."""
@@ -18,9 +24,24 @@ def solve(puzzle_input):
 
     return solution1, solution2
 
+
 if __name__ == "__main__":
     for path in sys.argv[1:]:
         print(f"{path}:")
         puzzle_input = pathlib.Path(path).read_text().strip()
         solutions = solve(puzzle_input)
         print("\n".join(str(solution) for solution in solutions))
+
+        # Automated submission
+        submission1 = input(f'Submit answer for part 1: {solutions[0]}? [y/N]: ')
+        if submission1.lower() != 'y':
+            print('Answer for part 1 not submitted.')
+        else:
+            submit(solutions[0], part='a', day=1, year=2023)
+
+        submission2 = input(f'Submit answer for part 2: {solutions[1]}? [y/N]: ')
+        if submission2.lower() != 'y':
+            print('Answer for part 2 not submitted.')
+        else:
+            submit(solutions[1], part='b', day=1, year=2023)
+            
