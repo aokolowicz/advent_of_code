e9d9ff5e304b21cfd87c6857fa056eb120ebf83b
Adrian <130671043+aokolowicz@users.noreply.github.com>
2023-12-22 13:28:08 +0100

    Add test_parse_example2()

--- 2023/test_aoc202300_template.py
+++ 2023/test_aoc202300_template.py
@@ -24,6 +24,12 @@ def test_parse_example1(example1):
 
 
 @pytest.mark.skip(reason="Not implemented")
+def test_parse_example2(example2):
+    """Test that input is parsed properly."""
+    assert example2 == ...
+
+
+@pytest.mark.skip(reason="Not implemented")
 def test_part1_example1(example1):
     """Test part 1 on example input."""
     assert aoc.part1(example1) == ...
